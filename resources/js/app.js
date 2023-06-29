47d3afbaf4c61d63e6d79dc93f778f14b84f62c2
dreamhigh9990 <dreamhigh9990@gmail.com>
2023-06-30 05:13:15 +0900

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,51 @@
+// app.js
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+import App from './App.vue';
+Vue.use(VueAxios, axios);
+
+import HomeComponent from './components/HomeComponent.vue';
+import CreateComponent from './components/CreateComponent.vue';
+import IndexComponent from './components/IndexComponent.vue';
+import EditComponent from './components/EditComponent.vue';
+import AddComponent from './components/AddComponent.vue';
+
+const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: HomeComponent
+    },
+    {
+        name: 'create',
+        path: '/create',
+        component: CreateComponent
+    },
+    {
+        name: 'posts',
+        path: '/posts',
+        component: IndexComponent
+    },
+    {
+        name: 'edit',
+        path: '/edit/:id',
+        component: EditComponent
+    },
+    {
+        name: 'add',
+        path: '/add',
+        component: AddComponent
+    },
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
